08e91c3634c2cbad54e99953c5130d58d1bdf1b2
Darlon <darloncosta050@gmail.com>
2026-03-29 21:47:22 -0300

    att

--- 01-fundamentos/conhecendo-jsx/index.jsx
+++ 01-fundamentos/conhecendo-jsx/index.jsx
@@ -1,6 +1,4 @@
 //  JSX é uma extensão da linguagem JavaScript
 // É o formato usado pelo React para representar os elementos do DOM
 
-const element = <h1>Hello, world</h1>
-
-
+const element = <h1>Hello, world</h1>;
