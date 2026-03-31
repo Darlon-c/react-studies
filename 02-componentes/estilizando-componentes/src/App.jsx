bf41ed85b5369aa58d4cbc282ff13ec1e60569f8
Darlon <darloncosta050@gmail.com>
2026-03-31 14:35:01 -0300

    react + tailwind

---
+++ 02-componentes/estilizando-componentes/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-gray-200 min-h-dvh flex justify-center items-center flex-col">
+        <h1 className="text-2xl font-semibold">Crie interfaces de usuário de componentes</h1>
+        <p>
+          React permite que você construa interfaces de usuário a partir de
+          pedaços individuais chamados componentes.
+        </p>
+       
+      </div>
+    </>
+  );
+}
