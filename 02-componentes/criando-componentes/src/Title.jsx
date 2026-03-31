159f7ad3a163d79ad77fcc43c478a8d5f385f016
Darlon <darloncosta050@gmail.com>
2026-03-31 12:20:53 -0300

    add tailwind

--- 02-componentes/criando-componentes/src/Title.jsx
+++ 02-componentes/criando-componentes/src/Title.jsx
@@ -1,5 +1,9 @@
 export default function Title() {
-  return <>
-  <h1>The library for web and native user interfaces</h1>
-  </>
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">
+        The library for web and native user interfaces
+      </h1>
+    </>
+  );
 }
