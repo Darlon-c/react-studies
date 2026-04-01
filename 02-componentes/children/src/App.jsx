4282004d4de1366c301a894a9dbce0956d7f0fb8
Darlon <darloncosta050@gmail.com>
2026-04-01 20:16:27 -0300

    children

---
+++ 02-componentes/children/src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from "./components/Profile.jsx";
+import picture from "/picture.avif";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        avatar={picture}
+        name="Darlon"
+        bio="Desenvolvedor Front-end"
+        email="darlon@email.com"
+        phone="(11) 99999-9999"
+        githubUrl="https://github.com/darlon-c"
+        linkedinUrl="https://www.linkedin.com/in/darloncosta050/"
+      ></Profile>
+    </>
+  );
+}
