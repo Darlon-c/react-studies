9838713c687929df9e745395bfd885534db98047
Darlon <darloncosta050@gmail.com>
2026-04-07 19:40:41 -0300

    att

--- 03-eventos-state/exc-gerador-de-senhas/src/components/Password.jsx
+++ 03-eventos-state/exc-gerador-de-senhas/src/components/Password.jsx
@@ -18,12 +18,35 @@ export default function Password() {
     setPass(result);
   }
 
+  function copy() {
+    if (!pass) {
+      alert("Não há senha para copiar");
+      return;
+    } else {
+      navigator.clipboard.writeText(pass);
+      alert("Senha copiada com sucesso!");
+    }
+  }
+
   return (
     <>
-      <div className="flex justify-center gap-4 py-24">
+      <div className="flex justify-center my-8 text-2xl font-bold">
+        <h1>Gerador de senhas</h1>
+      </div>
+      <div className="flex justify-center gap-4 py-12">
         <span className="font-semibold text-center">{pass}</span>
-        <button className="px-2 py-1 border rounded bg-emerald-500 hover:bg-emerald-700" onClick={generatePassword}>Gerar</button>
-        <button className="px-2 py-1 border rounded bg-amber-500 hover:bg-amber-700">Copiar</button>
+        <button
+          className="px-2 py-1 border rounded bg-emerald-500 hover:bg-emerald-700"
+          onClick={generatePassword}
+        >
+          Gerar
+        </button>
+        <button
+          className="px-2 py-1 border rounded bg-amber-500 hover:bg-amber-700"
+          onClick={copy}
+        >
+          Copiar
+        </button>
       </div>
     </>
   );
