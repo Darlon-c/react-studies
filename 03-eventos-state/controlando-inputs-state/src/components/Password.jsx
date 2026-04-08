39ad2ba1136b0ab2a863f10a780fb5b3d223dd61
Darlon <darloncosta050@gmail.com>
2026-04-08 10:36:57 -0300

    controlando inputs com state

---
+++ 03-eventos-state/controlando-inputs-state/src/components/Password.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export default function Password() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$";
+
+  const [pass, setPass] = useState("");
+  const [textCopy, setTextCopy] = useState("Copiar");
+  const [passwordSize, setPasswordSize] = useState(8);
+
+  function generatePassword() {
+    let result = "";
+
+    for (let i = 0; i < passwordSize; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+
+    setPass(result);
+    setTextCopy("Copiar");
+  }
+
+  function copy() {
+    if (!pass) {
+      alert("Não há senha para copiar");
+      return;
+    } else {
+      navigator.clipboard.writeText(pass);
+      setTextCopy("Copiado!");
+      alert("Senha copiada com sucesso!");
+    }
+  }
+
+  return (
+    <>
+      <div className="flex justify-center my-8 text-2xl font-bold">
+        <h1>Gerador de senhas</h1>
+      </div>
+      <div className="flex justify-center gap-4 py-12">
+        <div className="flex gap-4 py-1 border rounded">
+          <label htmlFor="passwordSize">Tamanho:</label>
+          <input
+            type="number"
+            min={1}
+            value={passwordSize}
+            className="text-center"
+            onChange={(ev) => setPasswordSize(Number(ev.target.value))} 
+          />
+        </div> <br />
+        <span className="font-semibold text-center">{pass}</span>
+        <button
+          className="px-2 py-1 border rounded bg-emerald-500 hover:bg-emerald-700"
+          onClick={generatePassword}
+        >
+          Gerar
+        </button>
+        <button
+          className="px-2 py-1 border rounded bg-amber-500 hover:bg-amber-700"
+          onClick={copy}
+        >
+          {textCopy}
+        </button>
+      </div>
+    </>
+  );
+}
