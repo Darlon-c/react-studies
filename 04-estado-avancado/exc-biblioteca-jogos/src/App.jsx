3a7ad6503b23199b7f0ae9c41175e115cfdff99a
Darlon <darloncosta050@gmail.com>
2026-04-16 11:15:39 -0300

    complete

--- 04-estado-avancado/exc-biblioteca-jogos/src/App.jsx
+++ 04-estado-avancado/exc-biblioteca-jogos/src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
 
 export default function App() {
-  const [games, setGames] = useState([])
+  const [games, setGames] = useState(() => {
+    const storageGames = localStorage.getItem("games");
+    if (!storageGames) return [];
+    return JSON.parse(storageGames);
+  });
+
   const [title, setTitle] = useState("");
   const [cover, setCover] = useState("");
 
   const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 1000000);
+    const id = Math.floor(Math.random() * 1000);
     const game = { id, title, cover };
-    setGames((state) => [...state, game]);
+    setGames((state) => {
+      const newState = [...state, game];
+      localStorage.setItem("games", JSON.stringify(newState));
+      return newState;
+    });
   };
 
   const handleSubmit = (ev) => {
     ev.preventDefault();
-    console.log({title, cover})
-    setTitle("")
-    setCover("")
+    addGame({ title, cover });
+    setTitle("");
+    setCover("");
+  };
+
+  const removeGame = (id) => {
+    setGames((state) => {
+      const newState = state.filter((game) => game.id !== id);
+      return newState;
+    });
   };
 
   return (
@@ -49,6 +65,17 @@ export default function App() {
             Adicionar
           </button>
         </form>
+        <div>
+          {games.map((game) => (
+            <div key={game.id}>
+              <img width="300px" src={game.cover} />
+              <div>
+                <h2>{game.title}</h2>
+                <button onClick={() => removeGame(game.id)}>Remover</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
