d8151aa052efa3d5e92990e296838198aea1b671
Darlon <darloncosta050@gmail.com>
2026-05-11 09:28:54 -0300

    useRef

---
+++ 04-estado-avancado/useRef/src/App.jsx
@@ -0,0 +1,34 @@
+import { useRef, useState } from "react";
+
+export default function App() {
+  let variable = 0;
+
+  const [state, setState] = useState(0);
+  const ref = useRef(0);
+
+  const showValue = () => {
+    alert(`
+      variável: ${variable}
+      ref: ${ref.current}
+      state: ${state} 
+      `);
+  };
+
+  return (
+    <>
+      <div>UseRef</div>
+      <hr />
+      <p>Variável: {variable}</p>
+      <p>Ref: {ref.current}</p>
+      <p>State: {state}</p>
+      <hr />
+      <button onClick={() => variable++}>Aumentar variável</button> <br />
+      <button onClick={() => ref.current++}>Aumentar ref</button> <br />
+      <button onClick={() => setState((state) => state + 1)}>
+        Aumentar state
+      </button>
+      <br /> <br />
+      <button onClick={showValue}>Mostrar valores</button>
+    </>
+  );
+}
